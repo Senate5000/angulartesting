c60cf3933286893893df9d516fdbceac96736555
Senate5000 <Senate5000@gmail.com>
2014-06-09 23:24:22 -0700

    adding sf4 test page

--- testground/urls.py
+++ testground/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import *
 
 urlpatterns = patterns('',
     (r'test', 'testground.views.testView'),
+    (r'sf4', 'testground.views.SF4'),
 
 )
--- testground/views.py
+++ testground/views.py
@@ -7,3 +7,7 @@ from django.template import RequestContext
 def testView(request):
 
     return render_to_response("eggheadscratch.html", context_instance=RequestContext(request))
+
+def SF4(request):
+
+    return render_to_response("sf4.html", context_instance=RequestContext(request))
