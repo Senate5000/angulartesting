cca3a991349f66ebe948cb79daaced9276511d58
Senate5000 <Senate5000@gmail.com>
2014-06-10 23:32:47 -0700

    adding more test files

--- static/js/sf4.js
+++ static/js/sf4.js
@@ -1,1 +1,14 @@
 var app = angular.module("app", []);
+
+app.controller("GameCtrl", function($http){
+    var t = this;
+    $http.get("/static/json/games.json")
+        .then(function(res){
+            t.games = res.data;
+        });
+
+
+    this.selectGame = function(game){
+        console.log(game)
+    };
+});
